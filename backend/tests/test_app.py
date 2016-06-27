1f8c2563e1149e23ff73dda8814ab7591e39b4ae
Kash <kash.karimi@gmail.com>
2016-06-27 13:21:48 +0100

    fix test structure

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,11 +1,16 @@
 import unittest
-from app import app
+
+from backend import app
 
 
-class TestCase(unittest.TestCase):
-    def setup(self):
-        app.config['TESTING'] = True
-        self.app = app.test_client()
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.create_app(debug=True)
+        self.app.config['TESTING'] = True
+
+    def test_test(self):
+        from backend.app import worker
+        pass
 
 
 if __name__ == '__main__':
